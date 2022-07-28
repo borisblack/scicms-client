1361b8bb281db9e9c0f47cbb27711662b6eb6398
Boris Chernysh <bchernysh@lanit.ru>
2022-07-28 21:44:57 +0700

    Remove unused associateBy method

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,10 +1,1 @@
 export const capitalizeFirstLetter = (str: string) => str.charAt(0).toUpperCase() + str.slice(1)
-
-export const associateBy = (arr: any[], key: string): {[name: string]: any} => {
-    const obj: {[name: string]: any} = {}
-    arr.forEach(it => {
-        obj[it[key]] = it
-    })
-
-    return obj
-}
