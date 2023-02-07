a8c6084dca50ac314b7db3209e66092d1a974921
Boris Chernysh <bchernysh@lanit.ru>
2023-02-07 21:21:08 +0700

    Move polarArea dash to Ant Charts

--- src/dashboard/dashes/PolarAreaDash.tsx
+++ src/dashboard/dashes/PolarAreaDash.tsx
@@ -1,56 +1,28 @@
-import {FC, useEffect, useMemo, useRef} from 'react'
-import Chart from 'chart.js/auto'
+import {FC} from 'react'
+import {Rose, RoseConfig} from '@ant-design/charts'
 
 import {DashType} from '../../types'
 import {InnerDashProps} from '.'
-import {mapLabels, mapMetrics} from '../../util/dashboard'
 
 const PolarAreaDash: FC<InnerDashProps> = ({pageKey, dash, fullScreen, data}) => {
     if (dash.type !== DashType.polarArea)
         throw new Error('Illegal dash type')
 
-    const {labelField} = dash
-    if (labelField == null)
+    const {metricField, labelField} = dash
+    if (metricField == null || labelField == null)
         throw new Error('Illegal argument')
 
-    const labels = useMemo(() => mapLabels(data, labelField), [data, labelField])
-    const preparedData = useMemo(() => mapMetrics(dash, data), [dash, data])
-    const canvasRef = useRef<HTMLCanvasElement>(null)
-
-    useEffect(() => {
-        const canvas = canvasRef.current
-        if (!canvas)
-            return
-
-        const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
-
-        const chart = new Chart(ctx, {
-            type: DashType.polarArea,
-            data: {
-                labels,
-                datasets: [{
-                    label: dash.name,
-                    data: preparedData,
-                    backgroundColor: [
-                        'rgb(255, 99, 132)',
-                        'rgb(75, 192, 192)',
-                        'rgb(255, 205, 86)',
-                        'rgb(201, 203, 207)',
-                        'rgb(54, 162, 235)'
-                    ]
-                }]
-            },
-            options: {
-                maintainAspectRatio: !fullScreen
-            }
-        })
-
-        return () => { chart.destroy() }
-    }, [dash.name, data, fullScreen, labels, preparedData])
-
-    return (
-        <canvas id={`${pageKey}#${dash.name}`} ref={canvasRef}/>
-    )
+    const config: RoseConfig = {
+        data,
+        xField: labelField,
+        yField: metricField,
+        seriesField: labelField,
+        radius: 0.9,
+        // legend: {
+        //     position: 'bottom',
+        // },
+    };
+    return <Rose {...config} />;
 }
 
 export default PolarAreaDash
