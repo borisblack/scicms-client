d0581a61f12f7090713409022f05e6676b1b2774
Boris Chernysh <chernyshba@iss-reshetnev.ru>
2023-04-04 11:48:50 +0700

    Add number filter between processing

--- src/bi/dash-filters/filter-value-fields/NumericFilterValueField.tsx
+++ src/bi/dash-filters/filter-value-fields/NumericFilterValueField.tsx
@@ -1,14 +1,14 @@
 import React, {FC} from 'react'
 import {FilterValueFieldProps} from './index'
 import {useTranslation} from 'react-i18next'
-import {Form, Input} from 'antd'
+import {Form, InputNumber} from 'antd'
 import styles from '../DashFilters.module.css'
 import {isNumeric} from '../../../util/bi'
 import {QueryOp} from '../../../types'
 
 const {Item: FormItem} = Form
 
-const NumericFilterValueField: FC<FilterValueFieldProps> = ({namePrefix, type, op}) => {
+const NumericFilterValueField: FC<FilterValueFieldProps> = ({form, namePrefix, type, op}) => {
     if (namePrefix.length === 0)
         throw new Error('Illegal argument')
 
@@ -18,18 +18,57 @@ const NumericFilterValueField: FC<FilterValueFieldProps> = ({namePrefix, type, o
     const fieldName = namePrefix[namePrefix.length - 1]
     const {t} = useTranslation()
 
-    if (op === QueryOp.$null || op === QueryOp.$notNull)
-        return null
+    function handleLeftValueChange(leftValue: number | null) {
+        const rightValue = form.getFieldValue([...namePrefix, 'extra', 'right']) ?? 0
+        form.setFieldValue([...namePrefix, 'value'], [leftValue ?? 0, rightValue])
+    }
 
-    return (
-        <FormItem
-            className={styles.formItem}
-            name={[fieldName, 'value']}
-            rules={[{required: true, message: ''}]}
-        >
-            <Input placeholder={t('Value')}/>
-        </FormItem>
-    )
+    function handleRightValueChange(rightValue: number | null) {
+        const leftValue = form.getFieldValue([...namePrefix, 'extra', 'left']) ?? 0
+        form.setFieldValue([...namePrefix, 'value'], [leftValue ?? 0, rightValue])
+    }
+
+    function renderContent() {
+        switch (op) {
+            case QueryOp.$null:
+            case QueryOp.$notNull:
+                return null
+            case QueryOp.$between:
+                return (
+                    <>
+                        <FormItem
+                            className={styles.formItem}
+                            name={[fieldName, 'extra', 'left']}
+                            rules={[{required: true, message: ''}]}
+                        >
+                            <InputNumber placeholder={t('Left value')} onChange={handleLeftValueChange}/>
+                        </FormItem>
+                        <FormItem
+                            className={styles.formItem}
+                            name={[fieldName, 'extra', 'right']}
+                            rules={[{required: true, message: ''}]}
+                        >
+                            <InputNumber placeholder={t('Right value')} onChange={handleRightValueChange}/>
+                        </FormItem>
+                    </>
+                )
+            case QueryOp.$in:
+            case QueryOp.$notIn:
+                return null
+            default:
+                return (
+                    <FormItem
+                        className={styles.formItem}
+                        name={[fieldName, 'value']}
+                        rules={[{required: true, message: ''}]}
+                    >
+                        <InputNumber placeholder={t('Value')}/>
+                    </FormItem>
+                )
+        }
+    }
+
+    return renderContent()
 }
 
 export default NumericFilterValueField
