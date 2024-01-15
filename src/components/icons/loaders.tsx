10ca925cc8bea427a8bff1fea2a0b8c05c606a97
Boris Chernysh <borisblack@mail.ru>
2024-01-15 10:15:08 +0700

    fix: Icons loading

--- src/components/icons/loaders.tsx
+++ src/components/icons/loaders.tsx
@@ -2,8 +2,8 @@ import * as antdIcons from '@ant-design/icons/lib/icons'
 import * as faIcons from 'react-icons/fa'
 import * as fa6Icons from 'react-icons/fa6'
 
-const allFaIcons: Record<string, any>  = {...faIcons, fa6Icons}
-const allIcons: Record<string, any> = {antdIcons, ...allFaIcons}
+const allFaIcons: Record<string, any>  = {...faIcons, ...fa6Icons}
+const allIcons: Record<string, any> = {...antdIcons, ...allFaIcons}
 export {antdIcons, allFaIcons, allIcons}
 
 export const loadAntdIcons = () => import('@ant-design/icons/lib/icons')
